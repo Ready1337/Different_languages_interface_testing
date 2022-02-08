cb3f72f8f8e5aaac40519a3961c8644f2b671bd4
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-02-09 03:05:11 +0500

    added assertion description

--- test_items.py
+++ test_items.py
@@ -12,4 +12,4 @@ def test_page_has_button_add_to_basket(browser):
     button = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(
         (By.CSS_SELECTOR, '.btn-add-to-basket')))
 
-    assert button != []
+    assert button != [], "Page dont have 'add to basket' button"
